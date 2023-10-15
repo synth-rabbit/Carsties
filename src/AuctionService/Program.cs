8d4d4d6e3f695f15683d8b257b546869fac304ea
Josh 'Panda' Edwards <josh.dean.edwards@gmail.com>
2023-10-15 13:05:04 -0500

    initial commit with microservices and and identity server example

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,49 @@
+using AuctionService.Data;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddDbContext<AuctionDbContext>(opts =>
+{
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddControllers();
+builder.Services.AddMassTransit(x =>
+{
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+
+    x.UsingRabbitMq((context,cfg)=>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,24 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Contracts;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+  public MappingProfiles()
+  {
+    CreateMap<Auction, AuctionDto>().IncludeMembers(m => m.Item);
+    CreateMap<Item, AuctionDto>();
+    CreateMap<CreateAuctionDto, Auction>()
+      .ForMember(d => d.Item, o => o.MapFrom(s => s));
+    CreateMap<CreateAuctionDto, Item>();
+    CreateMap<UpdateAuctionDto, Auction>()
+      .ForMember(d => d.Item, o => o.MapFrom(s => s));
+    CreateMap<UpdateAuctionDto, Item>();
+    CreateMap<AuctionDto, AuctionCreated>();
+    CreateMap<Auction, AuctionUpdated>().IncludeMembers(a => a.Item);
+    CreateMap<Item, AuctionUpdated>();
+  }
+}
